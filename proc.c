5690b48fac6916f9f9222d627cce6329c3195eaf
Myriam Hafez <myriam.hafez@gmail.com>
2026-04-22 17:22:15 +0300

    revert

--- proc.c
+++ proc.c
@@ -326,32 +326,33 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-
+  
   for(;;){
+    // Enable interrupts on this processor.
     sti();
 
+    // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-
-    struct proc *top = 0;
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
 
-      if(top == 0 || p->priority < top->priority) top = p;
-      
-    }
-    if(top != 0){
-      p = top;
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
-      swtch(&c->scheduler, p->context);
+
+      swtch(&(c->scheduler), p->context);
       switchkvm();
 
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
       c->proc = 0;
     }
-
     release(&ptable.lock);
+
   }
 }
 
