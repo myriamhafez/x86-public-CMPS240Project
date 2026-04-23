2eb7efc9d6a985902bdd9ca5c134c423b75bc1c0
myriamhafez <myriam.hafez@gmail.com>
2026-04-23 11:42:06 +0300

    Refactor tree function

--- tree.c
+++ tree.c
@@ -3,44 +3,55 @@
 #include "user.h"
 #include "fs.h"
 
-void  tree(char *path){
-int fd;
-struct dirent de;
-struct stat st;
-char fileName[512];
-char *p;
+void tree(char *path){
+  int fd;
+  struct dirent de;
+  struct stat st;
+  char fileName[512];
+  char *p;
 
-fd = open(path, 0);
+  if((fd = open(path, 0)) < 0){
+    printf("cannot open %s\n", path);
+    return;
+  }
 
-while(read(fd, &de, sizeof(de))==sizeof(de)){
-if(de.inum == 0) continue;
+  while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    if(de.inum == 0)
+      continue;
 
-char name[DIRSIZ+1];
-memmove(name, de.name, DIRSIZ);
-name[DIRSIZ]=0;
+    char name[DIRSIZ+1];
+    memmove(name, de.name, DIRSIZ);
+    name[DIRSIZ] = 0;
 
-if(strcmp(name, ".") == 0 || strcmp(name, "..") == 0) continue;
+    if(strcmp(name, ".") == 0 || strcmp(name, "..") == 0)
+      continue;
 
-strcpy(fileName, path);
-p = fileName + strlen(fileName);
-*p++ = '/';
-memmove(p, name, DIRSIZ);
-p[DIRSIZ] =0;
+    strcpy(fileName, path);
+    p = fileName + strlen(fileName);
+    *p++ = '/';
 
-if(stat(fileName, &st) <0) continue;
+    memmove(p, name, strlen(name));
+    p[strlen(name)] = 0;
 
-printf("%s\n", fileName);
+    if(stat(fileName, &st) < 0)
+      continue;
 
-if(st.type == T_DIR) tree(fileName);
+    printf("%s\n", fileName);
 
+    if(st.type == T_DIR)
+      tree(fileName);
+  }
+
+  close(fd);
 }
-close(fd);
-}
+
 int main(int argc, char *argv[]){
-printf("%s", "tree getting started \n");
-if(argc < 2)
-tree(".");
-else
-tree(argv[1]);
-exit();
+  printf("tree getting started\n");
+
+  if(argc < 2)
+    tree(".");
+  else
+    tree(argv[1]);
+
+  exit();
 }
