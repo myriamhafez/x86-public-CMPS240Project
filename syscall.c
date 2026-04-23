6cb0ec9b286856c2caa0e63b1c4edd703b491c5c
myriamhafez <myriam.hafez@gmail.com>
2026-04-24 00:26:33 +0300

    Add sys_clone and sys_join to syscall table

--- syscall.c
+++ syscall.c
@@ -104,6 +104,9 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_getProcCount(void);
+extern int sys_clone(void);
+extern int sys_join(void);
+ 
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +131,8 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getProcCount] sys_getProcCount,
+[SYS_clone]   sys_clone,
+[SYS_join]    sys_join,
 };
 
 void
