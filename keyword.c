55e807e386fe78af1a9527ea80acdbcb2b144b61
Myriam Hafez <myriam.hafez@gmail.com>
2026-04-25 16:03:39 +0300

    more fixes to keyword.c

--- keyword.c
+++ keyword.c
@@ -4,12 +4,12 @@
 #include "user.h"
 #include "fs.h"
 
-void findKeyword(char fileName, char word){
+void findKeyword(char *fileName, char *word){
 int fd;
 char lineBuf[1024];
 char wordBuf[512];
 int lineNum = 0;
-ssize_t bytes;
+int  bytes;
 int word_len = 0;
 if((fd = open(fileName, O_RDONLY)) < 0){
 printf(1, "failed to open %s\n", fileName);
@@ -17,22 +17,24 @@ return;
 }
 
 while((bytes = read(fd, lineBuf,sizeof(lineBuf))) > 0){
-for(ssize_t i = 0; i < bytes; i++) {
+for(int i = 0; i < bytes; i++) {
 char c = lineBuf[i];
 
 if(c == '\n') lineNum++;
 if(c  == ' '){
 
 if(word_len > 0) {
-wordBuf[word_len] == '\0';
+wordBuf[word_len] = '\0';
 
 if(strcmp(word, wordBuf) == 0){
 printf(1, "%s found at line %d",word, lineNum );
 }
 word_len = 0;
+}
 } else {
 if(word_len < 511){
 wordBuf[word_len] = c;
+word_len++;
 }
 }
 }
@@ -45,10 +47,10 @@ close(fd);
 
 }
 
-int main(int argc, char argv[]){
+int main(int argc, char *argv[]){
 if(argc !=  3){
 printf(1, "please use format keyword <filename> <word>");
 } else {
-keyword(argv[2], argv[3]);
+findKeyword(argv[1], argv[2]);
 }
 }
