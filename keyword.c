31ce11c17a25931402854fa0e3981362ca9b9bde
Myriam Hafez <myriam.hafez@gmail.com>
2026-04-25 15:54:52 +0300

    fixes to keyword

--- keyword.c
+++ keyword.c
@@ -4,36 +4,51 @@
 #include "user.h"
 #include "fs.h"
 
-char findKeyword(char fileName, char word){
+void findKeyword(char fileName, char word){
 int fd;
 char lineBuf[1024];
+char wordBuf[512];
 int lineNum = 0;
+ssize_t bytes;
+int word_len = 0;
 if((fd = open(fileName, O_RDONLY)) < 0){
 printf(1, "failed to open %s\n", fileName);
 return;
 }
 
-FILE *fp = fdopen(fp, "r");
-if(!fp) return;
+while((bytes = read(fd, lineBuf,sizeof(lineBuf))) > 0){
+for(ssize_t i = 0; i < bytes; i++) {
+char c = lineBuf[i];
 
-while(fgets(lineBuf, sizeof(lineBuf), fp)){
-lineNum++;
+if(c == '\n') lineNum++;
+if(c  == ' '){
 
-char *token = strtok(lineBuf, ",");
+if(word_len > 0) {
+wordBuf[word_len] == '\0';
 
-while(token != NULL){
-if(strcmp(token, word) == 0){
+if(strcmp(word, wordBuf) == 0){
 printf(1, "%s found at line %d",word, lineNum );
+}
+word_len = 0;
+} else {
+if(word_len < 511){
+wordBuf[word_len] = c;
+}
+}
+}
+
 
 }
 }
-}
+
+close(fd);
+
 }
 
 int main(int argc, char argv[]){
-if(argc !=  2){
-printf("please use format keyword <filename> <word>");
+if(argc !=  3){
+printf(1, "please use format keyword <filename> <word>");
 } else {
-keyword(argv[1], argv[2]);
+keyword(argv[2], argv[3]);
 }
 }
